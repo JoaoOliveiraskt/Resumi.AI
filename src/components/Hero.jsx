0f8e3b41d56b1d71ae4994d01ee6601aee5e5151
João Victor Oliveira <joaoliveiraa7@gmail.com>
2023-09-09 18:35:01 -0300

    feat: add .env rapidApiHost

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,31 @@
-import { logo } from '../assets';
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img src={logo} alt="sumz_logo" 
-        className='w-28 object-contain' />
+        <img src={logo} alt="sumz_logo" className="w-40 object-contain" />
 
-        <button  type='button'
-        onClick={() => window.open('https://github.com/joaooliveiraskt')}
-        className='black_btn'>
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/joaooliveiraskt/Summize-AI")}
+          className="black_btn"
+        >
           Github
         </button>
       </nav>
 
-      <h1 className='head_text'>
+      <h1 className="head_text tracking-wider">
         Resuma <br /> Artigos com <br />
-        <span className='sunset_gradient'>OpenAI GPT-4</span>
+        <span className="sunset_gradient">OpenAI GPT-4</span>
       </h1>
-      <h2 className="desc">
-        Simplifique sua leitura com o Summize: um resumidor de artigos de código aberto que simplifica textos longos em resumos diretos e compreensíveis
+      <h2 className="desc tracking-wider">
+        Transforme páginas longas em breves insights com o Resume.AI. Nosso
+        assistente virtual de resumos simplifica qualquer texto, basta fornecer
+        a URL e deixar o Resume.AI fazer o trabalho pesado.
       </h2>
     </header>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/services/article.js
+++ src/services/article.js
@@ -1,20 +1,21 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
+const rapidApiHost = import.meta.env.VITE_RAPID_API_HOST;
 
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
     prepareHeaders: (headers) => {
         headers.set('X-RapidAPI-Key', rapidApiKey)
-        headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com')
+        headers.set('X-RapidAPI-Host', rapidApiHost)
         
         return headers;
         }
     }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-         query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3&lang=pt`, 
+         query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=6&lang=pt`, 
         })
     }), 
 })
