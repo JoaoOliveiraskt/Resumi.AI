6a4313b630f96ded08d7385e396f6596245d7519
João Victor Oliveira <joaoliveiraa7@gmail.com>
2023-09-09 03:08:09 -0300

    feat: add style and initial components

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import { logo } from '../assets';
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" 
+        className='w-28 object-contain' />
+
+        <button  type='button'
+        onClick={() => window.open('https://github.com/joaooliveiraskt')}
+        className='black_btn'>
+          Github
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        Resuma <br /> Artigos com <br />
+        <span className='sunset_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplifique sua leitura com o Summize: um resumidor de artigos de código aberto que simplifica textos longos em resumos diretos e compreensíveis
+      </h2>
+    </header>
+  )
+}
+
+export default Hero
